2343e250fde3d0b1cee1c5bd24432256d1d08453
Rob N ★ <robn@robn.io>
2021-02-09 23:54:00 +1100

    first cut server

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,37 @@ mod message;
 mod channel;
 mod session;
 mod world;
+mod server;
 
 use crate::traits::Receiver;
 use crate::world::World;
+use crate::server::Server;
 use crate::channel::Channel;
 use crate::session::Session;
 use crate::message::Message;
 
-fn main() {
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
   let mut world = World::new();
 
+  let mut server = Server::new()?;
+
+  loop {
+    let messages = server.pump()?;
+
+    println!("{:?}", messages);
+
+    /*
+    for &m in messages.iter() {
+      if let Some(s) = world.get_session(m.from()) {
+        s.receive(&world, m);
+      }
+    }
+    */
+  }
+
+/*
   let c = Channel::new();
   let c_id = c.id();
 
@@ -36,4 +57,5 @@ fn main() {
   if let Some(c) = world.get_channel(c_id) {
     c.receive(&world, s1_id, &m);
   }
+*/
 }
---
+++ src/server.rs
@@ -0,0 +1,93 @@
+use mio::net::{TcpListener, TcpStream};
+use mio::{Events, Interest, Poll, Token};
+use std::collections::HashMap;
+use std::io::{self, Read};
+
+#[derive(Debug)]
+pub enum ServerMessage {
+  Connect(Token),
+  Disconnect(Token),
+  Read(Token, Vec<u8>),
+}
+
+pub struct Server {
+  poll:        Poll,
+  listener:    TcpListener,
+  token:       Token,
+  connections: HashMap<Token,TcpStream>,
+}
+
+impl Server {
+  pub fn new() -> io::Result<Server> {
+    let addr = "127.0.0.1:3000".parse().unwrap();
+
+    let mut server = Server {
+      poll:        Poll::new()?,
+      listener:    TcpListener::bind(addr)?,
+      token:       Token(1),
+      connections: HashMap::new(),
+    };
+
+    server.poll.registry().register(&mut server.listener, Token(0), Interest::READABLE)?;
+
+    Ok(server)
+  }
+
+  pub fn pump(&mut self) -> io::Result<Vec<ServerMessage>> {
+    let mut messages: Vec<ServerMessage> = vec!();
+
+    let mut events = Events::with_capacity(128);
+
+    self.poll.poll(&mut events, None)?;
+
+    for event in events.iter() {
+      match event.token() {
+
+        // listening socket
+        Token(0) => match self.listener.accept() {
+
+          // no more connections, go for next event
+          Err(e) if e.kind() == io::ErrorKind::WouldBlock => break,
+
+          // propogate any other error
+          Err(e) => return Err(e),
+
+          // new connection
+          Ok((mut conn, addr)) => {
+            println!("accepted connection: {}", addr);
+
+            self.token = Token(self.token.0 + 1);
+            self.poll.registry().register(&mut conn, self.token, Interest::READABLE.add(Interest::WRITABLE))?;
+            self.connections.insert(self.token, conn);
+
+            messages.push(ServerMessage::Connect(self.token));
+          }
+        },
+
+        // activity on user conn
+        token => {
+          if event.is_readable() {
+            if let Some(conn) = self.connections.get_mut(&token) {
+              let mut buf = vec![0; 4096];
+              match conn.read(&mut buf) {
+                Ok(0) => {
+                  // disconnected
+                  println!("disconnected: {}", conn.peer_addr()?);
+                  self.connections.remove(&token);
+                  messages.push(ServerMessage::Disconnect(token));
+                },
+                Ok(n) => {
+                  messages.push(ServerMessage::Read(token, buf));
+                },
+                Err(e) if e.kind() == io::ErrorKind::WouldBlock || e.kind() == io::ErrorKind::Interrupted => {},
+                Err(e) => return Err(e),
+              }
+            }
+          }
+        }
+      };
+    }
+
+    Ok(messages)
+  }
+}
